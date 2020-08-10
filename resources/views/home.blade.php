8380ab753a0f85432fc542a713aa3af8372003f0
MuhammadIbnu <m.ibnu96@gmail.com>
2020-08-10 14:53:05 +0700

    update hapus dinkes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,18 +52,7 @@
                       <!-- /.info-box -->
                     </div>
                     <!-- /.col -->
-                    <div class="col-md-3 col-sm-6 col-xs-12">
-                      <div class="info-box">
-                        <span class="info-box-icon bg-yellow"><i class="ion ion-ios-person-outline"></i></span>
-                  
-                        <div class="info-box-content">
-                          <span class="info-box-text">Petugas Dinkes</span>
-                        <span class="info-box-number">{{$dinkes}}</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                      </div>
-                      <!-- /.info-box -->
-                    </div>
+
                     <!-- /.col -->
                   </div>
             </div>
